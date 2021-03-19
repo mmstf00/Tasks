e5ae0854e62e5c070c72b1e06099dcda96ae40ad
Mustafa Ercan <mustafa.ercan392@gmail.com>
2021-03-19 16:49:18 +0200

    Task

---
+++ Task 1/Task 1/main.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+using namespace std;
+
+class Arrays {
+
+private:
+	int element;
+	float squareRoot[30], sum[30] = {};
+	int n, number[30], num[30], max;
+
+public:
+	void sumSquareRootOfEvens();
+	void theBiggestElement();
+	void getData();
+};
+
+//Getting data for amount of elements and the array of numbers.
+void Arrays::getData() {
+	cout << "Write amount of elements: ";
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Element number " << i + 1 << endl;
+		cin >> number[i];
+	}
+}
+
+//Getting the square root of each element and sum of them.
+void Arrays::sumSquareRootOfEvens() {
+
+
+	for (int i = 0; i < n; i++)
+	{
+		num[i] = number[i]; //Saving the elements in new array to work with them.
+	}
+
+	for (int i = 0; i < n; i++) {
+		while (num[i] > 0) {
+			element = num[i] % 10;                //Getting the last digit of element.
+			if (element % 2 == 0) {              //Check if its even.
+				squareRoot[i] = sqrt(element);  //Finding the square root and saving the element in new array.
+				sum[i] += squareRoot[i];       //Saving the summation of the square roots in new array.
+			}
+			num[i] = num[i] / 10;            //Deleting the last digit to move on the next.
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		cout << "Sum and square root even digits in element " << i + 1 << " = " << sum[i] << endl;
+	}
+}
+
+void Arrays::theBiggestElement() {
+
+	max = number[0];
+	for (int i = 1; i < n; i++) {
+		if (number[i] > max) {
+			max = number[i];
+		}
+	}
+	cout << max;
+
+}
+
+
+int main() {
+
+	Arrays A;
+
+	A.getData();
+	A.sumSquareRootOfEvens();
+	cout << "The biggest element is: ";
+	A.theBiggestElement();
+	cout << endl;
+}
---
+++ Task 2/Task 2/main.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+struct Triangle {
+	float sideA[20], sideB[20], sideC[20], P[20] /*Perimeter of triangle*/, faceTriangle[20];
+}T[20];
+
+struct Rectangle {
+	float sideA[20], sideB[20], faceOfRectangle[20];
+}R[20];
+
+
+//Reading information for Triangle.
+void ReadDataTriangle(int amount) {
+	for (int i = 0; i < amount; i++) {
+		cout << "Triangle number " << i + 1 << " Side A -> "; cin >> T[i].sideA[i];
+		cout << "Triangle number " << i + 1 << " Side B -> "; cin >> T[i].sideB[i];
+		cout << "Triangle number " << i + 1 << " Side C -> "; cin >> T[i].sideC[i];
+		cout << endl;
+	}
+}
+
+//Reading information for Rectangle.
+void ReadDataRectangle(int amount) {
+	for (int i = 0; i < amount; i++) {
+		cout << "Rectangle number " << i + 1 << " Side A -> "; cin >> R[i].sideA[i];
+		cout << "Rectangle number " << i + 1 << " Side B -> "; cin >> R[i].sideB[i];
+		cout << endl;
+	}
+}
+
+//Calculating face of Triangle with Heron formula.
+void Heron(int amount) {
+	for (int i = 0; i < amount; i++) {
+		//P = (a+b+c)/2 - Perimeter of the Triangle.
+		T[i].P[i] = (T[i].sideA[i] + T[i].sideB[i] + T[i].sideC[i]) / 2;
+	}
+	//S = sqrt(p*(p-a)*(p-b)*(p-c)) - Formula for face.
+	for (int i = 0; i < amount; i++) {
+		T[i].faceTriangle[i] = sqrt(T[i].P[i] * (T[i].P[i] - T[i].sideA[i]) * (T[i].P[i] - T[i].sideB[i]) * (T[i].P[i] - T[i].sideC[i]));
+	}
+}
+
+//Calculating face of Right-Anled Triangle
+void RightAngled(int amount) {
+	//S = (a*b)/2 - Formula for Face.
+	for (int i = 0; i < amount; i++) {
+		R[i].faceOfRectangle[i] = ((R[i].sideA[i]) * (R[i].sideB[i])) / 2;
+	}
+}
+
+//Finding the biggest Triangle face.
+void BiggestTriangleFace(int amount) {
+	double max = T[0].faceTriangle[0];
+	int a = 0;
+	for (int i = 0; i < amount; i++) {
+		if (T[i].faceTriangle[i] > max) {
+			max = T[i].faceTriangle[i];
+			a = i;
+		}
+	}
+	cout << "Biggest Triangle is: " << a + 1 << " with face " << max << endl;
+}
+
+//Finding the biggest Rectangle face.
+void BiggestRectangleFace(int amount) {
+	double max = R[0].faceOfRectangle[0];
+	int b = 0;
+	for (int i = 0; i < amount; i++) {
+		if (R[i].faceOfRectangle[i] > max) {
+			max = R[i].faceOfRectangle[i];
+			b = i;
+		}
+	}
+	cout << "Biggest Rectangle is: " << b + 1 << " with face " << max << endl;
+}
+
+int main() {
+	int amountTriangles, amountRectangles;
+	cout << "Enter amount Triangles: "; cin >> amountTriangles;
+	cout << "Enter amount Rectangles: "; cin >> amountRectangles;
+	cout << endl;
+
+	ReadDataTriangle(amountTriangles);
+
+	ReadDataRectangle(amountRectangles);
+
+	Heron(amountTriangles);
+
+	RightAngled(amountRectangles);
+
+	BiggestTriangleFace(amountTriangles);
+
+	BiggestRectangleFace(amountRectangles);
+}
